ae3447c084eb845c2cf23751e8fbae6267702eb2
Elrik <Elrik@DESKTOP-ULLMPFS>
2022-11-10 11:07:33 +0530

    created project - firstcommit

---
+++ src/com/faith/app/DemoAppv2022.java
@@ -0,0 +1,11 @@
+package com.faith.app;
+
+public class DemoAppv2022 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test App");
+	}
+
+}
